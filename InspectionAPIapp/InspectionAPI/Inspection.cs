44e521029bf6dda4ca64562f57a5be243e5019bc
Pazos Damian <damian_ariel_pazos@hotmail.com>
2022-06-22 10:28:05 -0300

    Class Propiedades

---
+++ InspectionAPIapp/InspectionAPI/Inspection.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InspectionAPI
+{
+    public class Inspection
+    {
+        
+        // Propiedades
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string Status { get; set; } = String.Empty;
+
+        [StringLength(200)]
+        public string Comments { get; set; } = String.Empty;
+
+        public int InspectionTypeId { get; set; }
+
+        public InspectionType? InspectionType { get; set; } // El signo de pregunta significa que se trata de un atributo anulable
+
+
+    }
+}
---
+++ InspectionAPIapp/InspectionAPI/InspectionType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InspectionAPI
+{
+    public class InspectionType
+    {
+        // Propiedades
+        public int Id { get; set; }
+
+        [StringLength(20)] // Especifica la longitud mínima y máxima de los caracteres permitidos en un campo de datos.
+        public string InspectionName { get; set; } = String.Empty; // Inicializa una variable de cadena en una cadena vacía (String.Empty).
+
+    }
+}
